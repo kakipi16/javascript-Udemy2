6dfaf9fefe397286ee152ba90b301cb238717851
kakipi16 <kakipi533@gmail.com>
2025-06-14 12:50:28 +0900

    030

--- 030_厳格な等価性と抽象的な等価性/start/main.js
+++ 030_厳格な等価性と抽象的な等価性/start/main.js
@@ -1,5 +1,25 @@
+function printEquality(a, b) {
+    console.log(a === b);
+    console.log(a == b );
+}
+
 let a = '1';
 let b = 1;
 
-console.log(a === b)
-console.log(a == b)
+
+// let c = true;
+// // console.log(b === c); false
+// // console.log(b == c ); true
+// printEquality(b, c);
+
+let e = "";
+let f = 0;
+let g = "0";
+
+printEquality(e, f);
+ // console.log(e === f); false
+ // console.log(e == f ); true
+
+printEquality(g, f);
+// // console.log(g === f); false
+// // console.log(g == f ); true

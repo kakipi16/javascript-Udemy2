251aa457ff18702969960a8e26b80d2d892f1f35
kakipi16 <kakipi533@gmail.com>
2025-06-18 17:59:57 +0900

    910学習 3/1 OK

--- 910_修了確認_falsyとtruthy/start/main.js
+++ 910_修了確認_falsyとtruthy/start/main.js
@@ -2,14 +2,23 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
+// //true
 // console.log("0 == false", 0 == false);
+// //false
 // console.log("0 === false", 0 === false);
+// //false
 // console.log('"false" == false', "false" == false);
+// //true
 // console.log('"0" == 0', "0" == 0);
+// //false
 // console.log('Boolean("0") === false', Boolean("0") === false);
+// // true
 // console.log('Boolean(0) === false', Boolean(0) === false);
+// //false
 // console.log('!Boolean(0) === false', !Boolean(0) === false);
+// //true **　false
 // console.log('-1 == false', -1 == false);
+// //true
 // console.log('!10 === false', !10 === false);
 
 /**
@@ -21,13 +30,29 @@
  * ※aには0以上の整数値が渡ってくるものとします。
  */
 let a = 1;
+// // ||を使って答えましょうの問題でした。
+// function fn(num) {
+//     num = num && -1;
+//     console.log(num);
+// }
+// fn(a);
 
+// //この記載方法だと今回のケースは間違いでは無いが初期値を与えずに書くのが今回の正解。
+
+// let b = null
+// function fn(num = -1) {
+//     console.log(num);
+// }
+// fn(b);
+
+//解答
 function fn(num) {
-    num = num || -1;
+    if(num === undefined || num === null){
+        num = -1;
+    }
     console.log(num);
 }
 fn(a);
-
 
 /**
  * 問題３：
@@ -39,3 +64,18 @@ fn(a);
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
+
+// ||を使いましょう！
+function greeting(name, grt = "hello") {
+    console.log(`${grt}, ${name}`);
+}
+
+greeting("Bob", "hi");
+
+//解答
+function greeting(name, hi) {
+    hi = hi || 'hello';
+    console.log(`${hi}, ${name}`)
+}
+
+greeting("Bob", "hi");
